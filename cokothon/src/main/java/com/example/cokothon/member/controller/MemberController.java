12c51e0f70d66180925db61332391d9a83ed71d2
nayoung16 <nykim1016@sookmyung.ac.kr>
2024-09-29 04:50:24 +0900

    [Feat] 내 정보 조회

--- cokothon/src/main/java/com/example/cokothon/member/controller/MemberController.java
+++ cokothon/src/main/java/com/example/cokothon/member/controller/MemberController.java
@@ -1,26 +1,58 @@
 package com.example.cokothon.member.controller;
 
+import com.example.cokothon.diary.dto.DiaryResponse;
+import com.example.cokothon.diary.entity.Diary;
+import com.example.cokothon.diary.repository.DiaryRepository;
+import com.example.cokothon.diary.service.DiaryService;
+import com.example.cokothon.member.dto.ProfileDto;
 import com.example.cokothon.member.entity.Member;
 import com.example.cokothon.member.service.MemberService;
+import com.example.cokothon.stress.dto.StressDto;
+import com.example.cokothon.stress.entity.Stress;
+import com.example.cokothon.stress.service.StressService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/my")
 @AllArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final DiaryRepository diaryRepository;
+    private final StressService stressService;
 
-//    @GetMapping()
-//    public ResponseEntity<?> getProfile(
-//            HttpServletRequest request
-//    ) {
-//        HttpSession session = request.getSession();
-//        Member memberInSession = (Member) session.getAttribute("logined");
-//    }
+    @GetMapping()
+    public ResponseEntity<?> getProfile(
+            HttpSession session
+    ) {
+        Member member = (Member) session.getAttribute("logined");
+        List<Diary> diaryList = diaryRepository.findAllById(member);
+        List<Stress> stressList = stressService.getAllStress(member.getMember_id());
+
+        List<DiaryResponse> diaryDtoList = diaryList.stream()
+                .map(diary -> new DiaryResponse(diary.getId(), diary.getTitle(), diary.getEmoji(),
+                        diary.getStress(), diary.getContent(), diary.getDate()))
+                .collect(Collectors.toList());
+
+        List<StressDto> stressDtoList = stressList.stream()
+                .map(stress -> new StressDto(stress.getMember().getMember_id(), stress.getStress()))
+                .collect(Collectors.toList());
+
+        ProfileDto profileDto = ProfileDto.builder()
+                .nickname(member.getNickname())
+                .diaryList(diaryDtoList)
+                .stressList(stressDtoList)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(profileDto);
+    }
 }
