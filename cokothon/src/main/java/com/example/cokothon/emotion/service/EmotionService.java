9b27ca42976efff856a88ecca15f929c4031b8ac
ljinny <leejieun2003@gmail.com>
2024-09-29 03:57:52 +0900

    Fix: member service 오류 해결 #20

---
+++ cokothon/src/main/java/com/example/cokothon/emotion/service/EmotionService.java
@@ -0,0 +1,2 @@
+package com.example.cokothon.emotion.service;public class EmotionService {
+}
--- cokothon/src/main/java/com/example/cokothon/member/service/MemberService.java
+++ cokothon/src/main/java/com/example/cokothon/member/service/MemberService.java
@@ -1,10 +1,13 @@
 package com.example.cokothon.member.service;
 
 import com.example.cokothon.member.dto.MemberDto;
+import com.example.cokothon.member.dto.MemberLoginDto;
 import com.example.cokothon.member.entity.Member;
 
 public interface MemberService {
     Member createMember(MemberDto memberDto);
 
     Member findByUserName(String userName) throws Exception;
+
+    boolean loginMember(MemberLoginDto memberLoginDto);
 }
