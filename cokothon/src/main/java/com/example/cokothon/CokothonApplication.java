b1408003802669f7c0885a4eee1a64a89c52b24b
nayoung16 <nykim1016@sookmyung.ac.kr>
2024-09-28 21:37:21 +0900

    init

---
+++ cokothon/src/main/java/com/example/cokothon/CokothonApplication.java
@@ -0,0 +1,13 @@
+package com.example.cokothon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokothonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CokothonApplication.class, args);
+    }
+
+}
---
+++ cokothon/src/test/java/com/example/cokothon/CokothonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cokothon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CokothonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
